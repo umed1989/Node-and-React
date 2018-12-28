241bfab1bbca20ee9a22eab4a9b18d1f2b15181c
umed tolibi <tolibiumed@gmail.com>
2018-12-28 18:02:42 -0500

    Setting up REdux

---
+++ client/src/components/App.js
@@ -0,0 +1,35 @@
+import "materialize-css/dist/css/materialize.min.css";
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import Header from "./Header";
+
+const Dashboard = () => <h1>I am Dashboard page</h1>;
+const SurveyNew = () => <h1>I am Servey new page</h1>;
+const Landing = () => <h1>I am Landing </h1>;
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" component={Landing} exact />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
